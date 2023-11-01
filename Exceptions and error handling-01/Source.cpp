963dbfe4568fff7f0cbd577e26948f7020ca20dd
Rom <kornev.roma1881@gmail.com>
2023-11-01 14:03:12 +0300

    Добавьте файлы проекта.

---
+++ Exceptions and error handling-01/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<Windows.h>
+#include <string>
+
+class bad_length {};
+
+int function(std::string str, int forbidden_length) {
+    int length = str.length();
+
+    if (length == forbidden_length) {
+        throw bad_length();
+    }
+
+    return length;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int forbidden_length;
+
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
+
+    std::string str;
+
+    try {
+        while (true) {
+            std::cout << "Введите слово: ";
+            std::cin >> str;
+
+            int length = function(str, forbidden_length);
+
+            std::cout << "Длина слова \"" << str << "\" равна " << length << std::endl;
+        }
+    }
+    catch (bad_length) {
+        std::cout << "Вы ввели слово запретной длины! До свидания" << std::endl;
+    }
+
+    return 0;
+}
